178dbeb8f52071d8404dff517127797b07243f53
Ron Yanku <ron.yanku86@gmail.com>
2018-11-08 20:35:43 +0200

    first commit

---
+++ src/main/java/eatseasyspring/eatseasyspring/EatsEasySpringApplication.java
@@ -0,0 +1,12 @@
+package eatseasyspring.eatseasyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatsEasySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatsEasySpringApplication.class, args);
+    }
+}
