8ed21ac445e3f2063692f4d85a841079bd55f431
frazikov <frazykov@gmail.com>
2018-12-23 22:13:46 +0200

    WIP: Dishes, Restaurant - redesigned

--- src/main/java/eatseasyspring/eatseasyspring/controller/RestaurantController.java
+++ src/main/java/eatseasyspring/eatseasyspring/controller/RestaurantController.java
@@ -1,42 +1,27 @@
 package eatseasyspring.eatseasyspring.controller;
 
-
-import eatseasyspring.eatseasyspring.model.Dish;
-import eatseasyspring.eatseasyspring.model.Restaurant;
-import eatseasyspring.eatseasyspring.repository.DishRepo;
-import eatseasyspring.eatseasyspring.repository.RestaurantRepo;
+import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import eatseasyspring.eatseasyspring.repository.DishRepo;
+import eatseasyspring.eatseasyspring.model.Restaurant;
+import eatseasyspring.eatseasyspring.repository.RestaurantRepo;
 
-@RequestMapping("/api")
+@RequestMapping("/api/restaurants")
 @CrossOrigin(origins = "*")
 @RestController
 public class RestaurantController {
-
     @Autowired
     private RestaurantRepo restRepo;
 
-    @Autowired
-    private DishRepo dishRepo;
-
-    @GetMapping(value = "restaurants")
+    @GetMapping(value = "")
     public Iterable<Restaurant> getAllRestaurants() {
         return restRepo.findAll();
     }
 
-    @GetMapping(value = "restaurants/{id}/menu")
-    public List<Dish> getMenu(@PathVariable("id") int id) {
-        return dishRepo.findDishesByRestaurant_RestaurantId(id);
-    }
-
-    // this is a template of how we could potentially add an instance to a table
-    // using a non default constructor would be quicker
-    @PostMapping(value = "restaurants/{id}/dish")
-    public Dish addDish(@PathVariable("id") int id, @RequestBody Dish dish) {
-        Optional<Restaurant> rest = restRepo.findById(id);
-        return dishRepo.save(dish);
+    @GetMapping(value = "/{id}")
+    public Optional<Restaurant> getRestaurantById(@PathVariable("restId") int restId) {
+        return restRepo.findById(restId);
     }
 }
--- src/main/java/eatseasyspring/eatseasyspring/model/User.java
+++ src/main/java/eatseasyspring/eatseasyspring/model/User.java
@@ -5,14 +5,60 @@ import javax.persistence.*;
 @Entity
 @Table(name = "user")
 public class User {
-
-
     @Id
-    @Column(name = "userID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
     private int userID;
 
+    @Column(name = "name")
+    private String name;
 
-    @Column(name = "userName")
-    private String userName;
+    @Column(name = "email")
+    private String email;
 
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "hashed_passwd")
+    private String hashedPasswd;
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getHashedPasswd() {
+        return hashedPasswd;
+    }
+
+    public void setHashedPasswd(String hashedPasswd) {
+        this.hashedPasswd = hashedPasswd;
+    }
 }
