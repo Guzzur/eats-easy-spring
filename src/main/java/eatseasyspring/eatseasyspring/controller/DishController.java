55aec998d18a5ea6d138096058bf9e0db2424c55
Felix Razikov <frazykov@gmail.com>
2018-12-28 11:50:41 +0200

    Done with Dish CRUD

--- src/main/java/eatseasyspring/eatseasyspring/controller/DishController.java
+++ src/main/java/eatseasyspring/eatseasyspring/controller/DishController.java
@@ -3,20 +3,16 @@ package eatseasyspring.eatseasyspring.controller;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import eatseasyspring.eatseasyspring.model.Dish;
 import eatseasyspring.eatseasyspring.repository.DishRepo;
-import eatseasyspring.eatseasyspring.model.Restaurant;
-import eatseasyspring.eatseasyspring.repository.RestaurantRepo;
 
 @RequestMapping("/api")
 @CrossOrigin(origins = "*")
 @RestController
 public class DishController {
-    @Autowired
-    private RestaurantRepo restRepo;
-
     @Autowired
     private DishRepo dishRepo;
 
@@ -35,5 +31,25 @@ public class DishController {
     @PostMapping(value = "dishes")
     public Dish addDish(@RequestBody Dish dish) {
         return dishRepo.save(dish);
+    }
+
+    // PUT routes
+    @PutMapping(value = "dishes/{dishId}")
+    public ResponseEntity<Dish> updateDish(@RequestBody Dish dish, @PathVariable int dishId) {
+        Optional<Dish> dishOptional = dishRepo.findById(dishId);
+
+        if(!dishOptional.isPresent())
+            return ResponseEntity.notFound().build();
+
+        dish.setDishId(dishId);
+        dishRepo.save(dish);
+
+        return ResponseEntity.ok(dish);
+    }
+
+    // DELETE routes
+    @DeleteMapping(value = "dishes/{dishId}")
+    public void deleteDish(@PathVariable int dishId) {
+        dishRepo.deleteById(dishId);
     }
 }
