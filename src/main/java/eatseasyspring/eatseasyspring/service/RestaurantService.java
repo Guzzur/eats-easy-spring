ff6966d1a3769c02462a474aaad03a62e4904917
Felix Razikov <frazykov@gmail.com>
2018-12-28 12:30:52 +0200

    Done with Users CRUD and a few another fixes

--- src/main/java/eatseasyspring/eatseasyspring/service/RestaurantService.java
+++ src/main/java/eatseasyspring/eatseasyspring/service/RestaurantService.java
@@ -4,11 +4,8 @@ import eatseasyspring.eatseasyspring.repository.RestaurantRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Iterator;
-
 @Service
 public class RestaurantService {
     @Autowired
-    private RestaurantRepo RestaurantRepository;
+    private RestaurantRepo restRepo;
 }
