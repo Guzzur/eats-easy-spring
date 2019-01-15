c2abdc3a5683f8a13c2142953a8da7e1ee525778
Ron Yanku <ron.yanku86@gmail.com>
2019-01-15 07:20:51 +0200

    working on restaurants/filtered

--- src/main/java/eatseasyspring/eatseasyspring/controller/RestaurantController.java
+++ src/main/java/eatseasyspring/eatseasyspring/controller/RestaurantController.java
@@ -2,8 +2,10 @@ package eatseasyspring.eatseasyspring.controller;
 
 import java.util.*;
 
+import com.github.tennaito.rsql.jpa.JpaCriteriaQueryVisitor;
 import cz.jirutka.rsql.parser.RSQLParser;
 import cz.jirutka.rsql.parser.ast.Node;
+import cz.jirutka.rsql.parser.ast.RSQLVisitor;
 import eatseasyspring.eatseasyspring.model.Dish;
 import eatseasyspring.eatseasyspring.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,9 @@ import org.springframework.web.bind.annotation.*;
 import eatseasyspring.eatseasyspring.repository.DishRepo;
 import eatseasyspring.eatseasyspring.model.Restaurant;
 import eatseasyspring.eatseasyspring.repository.RestaurantRepo;
+
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaQuery;
 
 @RequestMapping("/api")
 @CrossOrigin(origins = "*")
@@ -26,17 +31,35 @@ public class RestaurantController {
     @Autowired
     private RestaurantService restaurantService;
 
+
+    EntityManager manager;
+    RSQLVisitor<CriteriaQuery<Restaurant>, EntityManager> visitor = new JpaCriteriaQueryVisitor<Restaurant>();
+
     // GET routes
     @GetMapping(value = "restaurants")
     public Iterable<Restaurant> getAllRestaurants() {
         return restRepo.findAll();
     }
 
-    @GetMapping(value = "restaurants/filtered")
+    @GetMapping(value = "restaurants/filtered/")
     public List<Restaurant> findAllByRsql(@RequestParam(value = "search") String search) {
+
+    // Parse a RSQL into a Node
+    Node rootNode = new RSQLParser().parse(search);
+
+    // Visit the node to retrieve CriteriaQuery
+    CriteriaQuery<Restaurant> query = rootNode.accept(visitor, manager);
+
+// Do all sort of operations you want with the criteria query
+        query.getOrderList();
+
+    // Execute and get results
+    return manager.createQuery(query).getResultList();
+
+/*    public List<Restaurant> findAllByRsql(@RequestParam(value = "search") String search) {
         Node rootNode = new RSQLParser().parse(search);
         Specification<Restaurant> spec = rootNode.accept(new CustomRsqlVisitor<Restaurant>());
-        return restRepo.findAll(spec);
+        return restRepo.findAll(spec);*/
     }
 
     @GetMapping(value = "restaurants/{restId}")
