595e11df199a19f57ea4a00cb885ee19a2d35688
Ron Yanku <ron.yanku86@gmail.com>
2019-04-16 19:32:57 +0300

    Added WS

---
+++ src/main/java/eatseasyspring/eatseasyspring/config/Greeting.java
@@ -0,0 +1,18 @@
+package eatseasyspring.eatseasyspring.config;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
