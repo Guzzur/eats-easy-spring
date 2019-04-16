878f09a0479c22e6857ef986a462c008402b5eda
Ron Yanku <ron.yanku86@gmail.com>
2019-04-16 18:55:05 +0300

    Added WS

--- src/main/java/eatseasyspring/eatseasyspring/controller/WebSocketController.java
+++ src/main/java/eatseasyspring/eatseasyspring/controller/WebSocketController.java
@@ -1,13 +1,58 @@
 package eatseasyspring.eatseasyspring.controller;
 
+import eatseasyspring.eatseasyspring.config.Greeting;
+import eatseasyspring.eatseasyspring.config.HelloMessage;
+import org.hibernate.mapping.Map;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.*;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
+import java.security.Principal;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.util.HtmlUtils;
+
+/*@Controller
+public class WebSocketController {
+
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+
+
+
+    @MessageMapping("/message")
+    @SendToUser("/queue/reply")
+    public String processMessageFromClient(
+            @Payload String message,
+            Principal principal) throws Exception {
+        return "4fd";
+    }
+
+}*/
+
+/*@Controller
+public class WebSocketController {
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+}*/
+
+/*
 @Controller
 public class WebSocketController {
 
@@ -23,4 +68,22 @@ public class WebSocketController {
 
         this.template.convertAndSend("/topic",  message);
     }
+}*/
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@CrossOrigin(origins = "*")
+@Controller
+public class WebSocketController {
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
 }
